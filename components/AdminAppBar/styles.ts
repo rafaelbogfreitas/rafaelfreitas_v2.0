57958c61c4d1ed64a8dec82488ed137f84d35d1c
Rafael Freitas <rafaelborges1985@hotmail.com>
2021-05-15 18:50:38 -0300

    Create app bar component for admin page

---
+++ components/AdminAppBar/styles.ts
@@ -0,0 +1,16 @@
+import { makeStyles } from '@material-ui/core';
+
+export const useStyles = makeStyles({
+  logo: {
+    width: 50,
+    marginRight: 20,
+  },
+  logoutBtn: {
+    backgroundColor: 'white',
+    color: 'orange',
+    marginLeft: 'auto',
+  },
+  appBar: {
+    backgroundColor: 'orange',
+  },
+});
--- pages/admin/index.tsx
+++ pages/admin/index.tsx
@@ -1,64 +1,39 @@
-import React, { useState } from 'react';
+import React from 'react';
 import Head from 'next/head';
-import { useSelector, useDispatch } from 'react-redux';
+import { useSelector } from 'react-redux';
 import { GetStaticProps } from 'next';
 import { useRouter } from 'next/router';
 import { RootState } from '../../store';
-import { logout } from '../../store/Auth.store';
 
 import connectToDb from '../../database/connectToDb';
 import { Project } from '../../database/models';
-
-//testing
-import { Button } from '@material-ui/core';
-import { axiosInstance } from '../../api/axios';
+import AdminAppBar from '../../components/AdminAppBar';
 interface ProjectsList {
   _id: string;
   title: string;
 }
-
 interface AdminHomeProps {
   projectsList: ProjectsList[];
 }
 
 const AdminHome = ({ projectsList }: AdminHomeProps): JSX.Element => {
   const { authToken } = useSelector((state: RootState) => state.auth);
-
-  const [projects, setProjects] = useState<ProjectsList[]>([]);
-
-  const dispatch = useDispatch();
   const router = useRouter();
 
   if (!authToken) {
     router.push('/admin/login');
   }
 
-  const getProjects = async (): Promise<void> => {
-    try {
-      const { data: projects } = await axiosInstance.get<ProjectsList[]>(
-        'projects/list',
-        {}
-      );
-      setProjects(projects);
-    } catch (err) {
-      console.error(err);
-    }
-  };
-
   return (
     <>
       <Head>
         <title>Admin | Home</title>
       </Head>
+      <AdminAppBar />
       <div>
         {projectsList.map((project, i) => (
           <div key={i}>{project.title}</div>
         ))}
-        {JSON.stringify(projects)}
-        <button type="button" onClick={() => dispatch(logout())}>
-          Logout
-        </button>
-        <Button onClick={getProjects}>Get Projects</Button>
       </div>
     </>
   );
