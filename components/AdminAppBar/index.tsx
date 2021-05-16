a4cfde9f51e38cf67f98064b48f3dd9960cbd65e
Rafael Freitas <rafaelborges1985@hotmail.com>
2021-05-16 12:02:30 -0300

    Implement add project functionality
    Add logic to handleSubmit for different methods and endpoints
    
    Implement create project endpoint
    
    Add new project button
    
    Render new project button conditionally
    
    push navigation to admin after creating project

--- components/AdminAppBar/index.tsx
+++ components/AdminAppBar/index.tsx
@@ -4,6 +4,8 @@ import { useDispatch } from 'react-redux';
 import { AppBar, Button, Toolbar, Typography } from '@material-ui/core';
 import { useStyles } from './styles';
 import { logout } from '../../store/Auth.store';
+import Link from 'next/link';
+import { useRouter } from 'next/router';
 
 interface AdminAppBarProps {
   title?: string;
@@ -12,6 +14,9 @@ interface AdminAppBarProps {
 const AdminAppBar = ({ title }: AdminAppBarProps): JSX.Element => {
   const classes = useStyles();
   const dispatch = useDispatch();
+  const router = useRouter();
+
+  const isNewProjectPage = router.pathname === '/admin/project/new';
 
   return (
     <AppBar position="sticky" className={classes.appBar}>
@@ -24,10 +29,17 @@ const AdminAppBar = ({ title }: AdminAppBarProps): JSX.Element => {
         <Typography className={classes.title} variant="body1">
           {title}
         </Typography>
+        {!isNewProjectPage && (
+          <Link href="/admin/project/new">
+            <Button variant="contained" className={classes.btnStyles}>
+              new project
+            </Button>
+          </Link>
+        )}
         <Button
           variant="contained"
           onClick={() => dispatch(logout())}
-          className={classes.logoutBtn}
+          className={classes.btnStyles}
         >
           logout
         </Button>
